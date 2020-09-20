e569455c73ef0322eed452ab282f46ad443bf4e5
chaehuiyuu <20172059@sungshin.ac.kr>
2020-09-21 03:38:41 +0900

    Revert "W03-P"
    This reverts commit 2f3b62d95804d110206dc84aa3dee88c55932c4a.

--- create.php
+++ create.php
@@ -1,41 +1,42 @@
 <?php
   $link = mysqli_connect('localhost','root','rootroot','dbp');
-  $query = "select * from drama";
+  $query = "select * from movie ";
   $result = mysqli_query($link, $query);
   $list = '';
   while($row = mysqli_fetch_array($result)){
-    $list =  $list."<li><a
-    href =\"index.php?id={$row['id']}\">{$row['title']}</a></li>";
+    $list = $list."<li><a
+    href=\"index.php?id={$row['id']}\">{$row['title']}</a></li>";
   }
 
-  $article = array(
-    'title' => 'DRAMA',
-    'description' => 'What is your favorite drama?'
-  );
+$article = array(
+  'title' => 'Welcome',
+  'description' => 'What is your favorite movie?'
+)
 
-if(isset($_GET['id']) ){
-  $query = "SELECT * FROM drama WHERE id={$_GET['id']}";
+if(isset($_GET['id'])){
+  $query = "select * from movie where id = {$_GET['id']}";
   $result = mysqli_query($link, $query);
   $row = mysqli_fetch_array($result);
   $article = array(
     'title' => $row['title'],
     'description' => $row['description']
-  );
-}
-?>
+    );
+  }
+ ?>
 
 <!DOCTYPE html>
 <html>
 <head>
   <meta charset="utf-8">
-  <title> DRAMA </title>
+  <title> MOVIE </title>
 </head>
 <body>
-  <h1>DRAMA</H1>
-  <ol><?= $list ?></ol>
+  <h1><a href = "index.php">MOVIE</h1>
+  <ol> <? = $list ?> </ol>
   <form action="process_create.php" method="post">
     <p><input type="text" name="title" placeholder="title"></p>
     <p><textarea name="description" placeholder="description"></textarea></p>
+    <p><input type="data" name=data></p>
     <p><input type="submit"></p>
   </form>
 </body>
