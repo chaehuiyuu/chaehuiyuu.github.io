0f0873952ab7b258855e082172327ce93bd71c8f
chaehuiyuu <20172059@sungshin.ac.kr>
2020-09-26 20:30:38 +0900

    W04-P

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
   $query = "select * from drama";
   $result = mysqli_query($link, $query);
   $list = '';
+
   while($row = mysqli_fetch_array($result)){
     $list =  $list."<li><a
     href =\"index.php?id={$row['id']}\">{$row['title']}</a></li>";
@@ -13,22 +14,28 @@
     'description' => '당신이 제일 좋아하는 드라마는?'
   );
   $update_link = '';
+  $actor = '';
+
   $delete_link = '';
 
 if(isset($_GET['id']) ){
-  $query = "SELECT * FROM drama WHERE id={$_GET['id']}";
+  $filtered_id = mysqli_real_escape_string($link,$_GET['id']);
+  $query = "SELECT * FROM drama left join actor on drama.actor_id = actor.id WHERE drama.id={$filtered_id}";
   $result = mysqli_query($link, $query);
   $row = mysqli_fetch_array($result);
-  $article = array(
-    'title' => $row['title'],
-    'description' => $row['description']
-  );
+  $article['title'] = htmlspecialchars($row['title']);
+  $article['description'] = htmlspecialchars($row['description']);
+  $article['name'] = htmlspecialchars($row['name']);
+
   $update_link = '<a href="update.php?id='.$_GET['id'].'">수정</a>';
-  $delete_link='<form action="process_delete.php" method="POST">
+  $delete_link='
+  <form action="process_delete.php" method="POST">
         <input type="hidden" name="id" value="'.$_GET['id'].'">
         <input type="submit" value="delete">
       </form>
       ';
+
+  $actor = "<p>by{$article['name']}</p>";
 }
 ?>
 
@@ -40,11 +47,13 @@ if(isset($_GET['id']) ){
 </head>
 <body>
   <h1><a href="index.php">MY FAVORITE DRAMA</a></H1>
+    <a href="actor.php">author</a>
   <ol><?= $list ?></ol>
   <a href="create.php">추가</a>
   <?=$update_link?>
   <?=$delete_link?>
   <h2><?= $article['title'] ?></h2>
   <?= $article['description'] ?>
+  <?= $actor ?>
 </body>
 </html>
